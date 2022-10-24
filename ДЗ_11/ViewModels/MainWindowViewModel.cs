fd2d74ed5a27762047b1e80acc2c90f599a322a9
Григорий Тарнавский <gabuska@inbox.ru>
2022-10-25 02:46:00 +0300

    Окна

--- ДЗ_11/ViewModels/MainWindowViewModel.cs
+++ ДЗ_11/ViewModels/MainWindowViewModel.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using ДЗ_11.ViewModels.Base;
 
 namespace ДЗ_11.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        private Page Authorization;
+        private Page Working;
+
+        private Page _currentPage;
+        public Page CurrentPage
+        {
+            get => _currentPage;
+            set => Set(ref _currentPage, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            Authorization = new Views.Pages.Authorization();
+            Working = new Views.Pages.Working();
+
+            CurrentPage = Authorization;
+        }
     }
 }
